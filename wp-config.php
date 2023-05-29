22bb17dee2469d9c30bd0b4ac7ceb0a7b5ddbb7c
Rahulsinghhh <rahulsingh3063@gmail.com>
2023-05-29 19:36:01 +0530

    enter data

--- wp-config.php
+++ wp-config.php
@@ -85,7 +85,7 @@ define('WP_DEBUG', false);
 /* Add any custom values between this line and the "stop editing" line. */
 
 
-define('WP_MEMORY_LIMIT', '256M');
+define('WP_MEMORY_LIMIT', '512M');
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
